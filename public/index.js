006d8357cc43ab6cdf7a4bbdc7959440c1888d2b
zenakhane <lilliantyiso@gmail.com>
2022-05-24 11:01:39 +0200

    make it better

--- public/index.js
+++ public/index.js
@@ -7,10 +7,17 @@ document.addEventListener('alpine:init', () => {
         garments : [],
         genderFilter: '',
         seasonFilter:'',
+        
         filterData(){
             fetch(`/api/garments?gender=${this.genderFilter}&season=${this.seasonFilter}`)
 			.then(r => r.json())
 			.then(data => this.garments = data.data );
         },
-    }))
+
+    filterPrices(){
+        fetch(`/api/garments?gender=${this.genderFilter}&season=${this.seasonFilter}`)
+        .then(r => r.json())
+        .then(data => this.garments = data.data );
+    },
+}))
 })
