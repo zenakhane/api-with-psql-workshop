f2429f539895df52469bec3cc507af7595f813af
zenakhane <lilliantyiso@gmail.com>
2022-05-27 11:58:29 +0200

    heroku heroku

--- index.js
+++ index.js
@@ -1,22 +1,20 @@
-const supertest = require('supertest');
 const PgPromise = require("pg-promise")
 const express = require('express');
-const assert = require('assert');
-const fs = require('fs');
 require('dotenv').config()
 
 const API = require('./api');
-const { default: axios } = require('axios');
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'))
 
-const config = {connectionString:process.env.DATABASE_URL || 
-	'postgres://zenakhane:codex123@localhost5432/garments',max:30,ssl:{
-	rejectUnauthorized : false
-}
+const config = {
+	connectionString: process.env.DATABASE_URL,
+	max: 30,
+	ssl: {
+		rejectUnauthorized: false
+	}
 };
 const pgp = PgPromise({});
 const db = pgp(config);
@@ -26,6 +24,6 @@ API(app, db);
 const PORT = process.env.PORT || 5000;
 
 
-app.listen(PORT, function() {
+app.listen(PORT, function () {
 	console.log(`App started on port ${PORT}`)
 });
