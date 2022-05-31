0e259dcd515d18476e65b72a2f0cccadd1126fec
zenakhane <lilliantyiso@gmail.com>
2022-05-31 15:39:22 +0200

    adding garments

--- public/index.js
+++ public/index.js
@@ -1,25 +1,40 @@
 document.addEventListener('alpine:init', () => {
     Alpine.data('app', () => ({
-        init(){
+        init() {
             this.filterData();
-        
-        },
-        garments : [],
-        genderFilter: '',
-        seasonFilter:'',
-        maxPrice:0,
-        filterData(){
-            fetch(`/api/garments?gender=${this.genderFilter}&season=${this.seasonFilter}`)
-			.then(r => r.json())
-			.then(data => this.garments = data.data );
-        },
-    filterPrices(){
-        console.log(this.maxPrice)
-        console.log(`/api/garments/${this.maxPrice}`)
-        fetch(`/api/garments/${this.maxPrice}`)
-        .then(r => r.json())
-        .then(data => this.garments = data.data );
-    },
-}))
 
-})
+        },
+        garms: {
+      description:'',
+        img:'',
+        price:0,
+        season:'',
+        gender:'',
+        },
+        garments: [],
+        genderFilter: '',
+        seasonFilter: '',
+        maxPrice: 0,
+        filterData() {
+            fetch(`/api/garments?gender=${this.genderFilter}&season=${this.seasonFilter}`)
+                .then(r => r.json())
+                .then(data => this.garments = data.data);
+        },
+        filterPrices() {
+            fetch(`/api/garments/${this.maxPrice}`)
+                .then(r => r.json())
+                .then(data => this.garments = data.data);
+        },
+        addGarment() {
+            fetch(`/api/garment/`,{
+                method: 'POST',
+            
+                headers:{
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({description, price, img, season, gender} = this)
+            })
+        }
+    })
+   
+)})
